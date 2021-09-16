917b9fbd4e09e54a1bd939fca9efa1382f05ac50
nabin <nabin043@gmail.com>
2021-09-16 21:31:54 +0545

    name

--- components/Project.model.js
+++ components/Project.model.js
@@ -8,7 +8,7 @@ export default [
   },
   {
     name: "day-2",
-    tittle: "portfolio",
+    tittle: "Hospital-Langin_page",
     tag: "Completed",
     link: "/p-2/home",
     info: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
