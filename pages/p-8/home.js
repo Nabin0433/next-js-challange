c2d72a473091acfeaed1b0f63b201581c2965480
nabin <nabin043@gmail.com>
2021-09-25 13:25:36 +0545

    p-8-start

--- pages/p-8/home.js
+++ pages/p-8/home.js
@@ -1,12 +1,8 @@
-import Particles from "react-tsparticles";
-import Connect from "../../components/animations/preactPartical/Connect";
-import Parallax from "../../components/animations/preactPartical/Parallax";
 import Twinkle from "../../components/animations/preactPartical/twinkle";
 
 const home = () => {
   return (
     <div className="bg-gray-700 h-screen">
-      {/* <Twinkle /> */}
       <div className="relative h-[80vh] w-[100vw] z-10 overflow-hidden">
         <Twinkle />
       </div>
