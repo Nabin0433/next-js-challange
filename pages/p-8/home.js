a443e66fbf40ed104a629e1c0c1ad30ee66dda52
nabin <nabin043@gmail.com>
2021-09-25 13:27:41 +0545

    p-8-start

--- pages/p-8/home.js
+++ pages/p-8/home.js
@@ -1,11 +1,7 @@
-import Twinkle from "../../components/animations/preactPartical/twinkle";
-
 const home = () => {
   return (
     <div className="bg-gray-700 h-screen">
-      <div className="relative h-[80vh] w-[100vw] z-10 overflow-hidden">
-        <Twinkle />
-      </div>
+      <div className="relative h-[80vh] w-[100vw] z-10 overflow-hidden"></div>
     </div>
   );
 };
